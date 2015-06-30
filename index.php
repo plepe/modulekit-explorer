bccac0ec43097d4de2fa0d04ebd1782942e0fbae
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-06-30 12:05:03 +0200

    Include submodules

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
+<?php include "conf.php"; /* load a local configuration */ ?>
+<?php include "modulekit/loader.php"; /* loads all php-includes */ ?>
+<?php call_hooks("init"); /* initialize submodules */ ?>
+
 <?php
-include "conf.php";
-
-include "inc/Explorer.php";
-include "inc/ExplorerPath.php";
-include "inc/basic_types.php";
-include "inc/basic_actions.php";
-
 $explorer = new Explorer($base_path);
 
 $ex_path = $explorer->get(array_key_exists('path', $_REQUEST ? $_REQUEST['path'] : null));
 $body = json_encode($ex_path->content());
+
+Header("Content-Type: text/html; charset=utf-8");
 ?>
 <!DOCTYPE HTML>
 <html>
   <head>
+    <title>Modulekit Explorer</title>
+    <?php print modulekit_to_javascript(); /* pass modulekit configuration to JavaScript */ ?>
+    <?php print modulekit_include_js(); /* prints all js-includes */ ?>
+    <?php print modulekit_include_css(); /* prints all css-includes */ ?>
+    <?php print_add_html_headers(); /* print additional html headers */ ?>
   </head>
   <body>
   <pre>
