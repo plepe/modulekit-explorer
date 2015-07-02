ed89a3aabeb992f4d3b28eb07fa161d4b1f28e13
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-07-02 11:11:52 +0200

    File Type: plaintext

---
+++ inc/ExplorerFileType_plaintext.php
@@ -0,0 +1,18 @@
+<?php
+class ExplorerFileType_plaintext extends ExplorerFileType {
+  public $mime_types = array("text/plain", "text/csv", "text/html", "application/xml", "text/x-shellscript", "text/x-php");
+
+  function view($file) {
+    $ret  = "<pre wrap>\n";
+
+    $content = $file->raw();
+
+    if($file->mime_encoding() == "iso-8859-1")
+      $content = utf8_encode($content);
+
+    $ret .= $content;
+    $ret .= "</pre>\n";
+
+    return $ret;
+  }
+}
--- inc/basic_file_types.php
+++ inc/basic_file_types.php
@@ -3,4 +3,5 @@ function explorer_register_basic_file_types($explorer) {
   $explorer->register_file_type('directory', 'ExplorerFileType_directory');
   $explorer->register_file_type('image', 'ExplorerFileType_image');
   $explorer->register_file_type('default', 'ExplorerFileType_default');
+  $explorer->register_file_type('default', 'ExplorerFileType_plaintext');
 }
